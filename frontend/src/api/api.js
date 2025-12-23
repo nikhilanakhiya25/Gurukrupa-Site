925976744b90ac3b0e996de88cfde206b53a97b2
nikhilanakhiya25 <nikhil.anakhyia1l@gmail.com>
2025-12-23 14:26:27 +0530

    Frontend side api.js file add render link

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://gurukrupa-site-giftarticle.onrender.com/api",
+  baseURL: "https://gurukrupa-site-guftarticle.onrender.com/api",
   headers: {
     "Content-Type": "application/json"
   },

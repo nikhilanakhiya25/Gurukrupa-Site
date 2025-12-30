c55d5bf92435439fc07fe22d9edcf1c27a5b9018
nikhilanakhiya25 <nikhil.anakhyia1l@gmail.com>
2025-12-30 18:09:15 +0530

    change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import OrderSuccess from "./components/OrderSucess";
 import OrderTracking from "./components/OrderTracking"; // Assuming this exists, or adjust
-=======
+
 import MyOrders from "./components/userOrders";
 import OrderSuccess from "./components/OrderSucess";
 import OrderTracking from "./components/OrderTracking"; // Assuming this exists, or adjust
